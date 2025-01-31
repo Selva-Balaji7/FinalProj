3eff660d4505f660da2bf33b8e22302095757c90
Selva-balaji7 <tharunbalaji1807@gmail.com>
2025-01-31 18:36:42 +0530

    save

---
+++ Attendance_management/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Attendance_management.Data;
+using Attendance_management.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Attendance_management.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Attendance>>> GetAdmin()
+        {
+            var entries = await _context.Users.ToListAsync();
+
+            return Ok(entries);
+        }
+    }
+}
---
+++ Attendance_management/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Attendance_management.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public string? ProfilePicture { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<Attendance> Attendances { get; set; } = new List<Attendance>();
+}
